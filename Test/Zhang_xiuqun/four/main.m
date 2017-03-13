31972c946b7328b7453ef4cdc19dd7ed18abf96a
Hanzhiyun <1160588449@qq.com>
2017-03-13 22:17:49 +0800

    画出了在测量点0点的关于x,y,z的磁感应强度的四维图像

---
+++ Test/Zhang_xiuqun/four/main.m
@@ -0,0 +1,24 @@
+function main
+% global data;
+format long
+% if isempty(data)
+%     filename = '0µã.xlsx';
+%     data = xlsread(filename);
+% end
+filename0 = '0µã.xlsx';
+filename3 = 'LTC-3m.xlsx';
+data0 = xlsread(filename0);
+data3 = xlsread(filename3);
+% [l, B] = input_lB(data);
+% fun = @f_B;
+% x0 = [0, 0, 0];
+% x = fsolve(fun, x0)
+
+
+test_fit(data0);
+test_fit(data3);
+% x = data(:,1:3);
+% B = data(:,4);
+% F = f_B(x) - B'
+% [~,idx] = find(abs(F) < 1)
+% emin = F(idx)
