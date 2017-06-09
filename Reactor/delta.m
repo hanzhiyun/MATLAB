be45f6692a7a655a3717b06458bc59d44ff740af
Hanzhiyun <1160588449@qq.com>
2017-06-09 10:18:29 +0800

    对于不同p的三环简化模型和原模型的空间磁场的误差分析

---
+++ Reactor/delta.m
@@ -0,0 +1,35 @@
+function delta = delta(N)
+% 求最优简化模型过程中的误差
+
+N = 100;
+% p = 34;
+delta_x = [];
+delta_z = [];
+Delta_x = [];
+Delta_z = [];
+
+for p = 1:N/2
+    for x = 0.064:0.002:0.2
+        B_o = B_origin(x, 0, 0, N);
+        B_r3 = B_ring3(x, 0, 0, N, p);
+        delta_x = [delta_x, abs(norm(B_o) - norm(B_r3))/norm(B_o)];
+    end
+    % figure
+    % x = 0.064:0.002:0.2;
+    % plot(x,delta_x);
+    Delta_x = [Delta_x, sum(delta_x)];
+    
+    for z = 0:0.002:0.2
+        B_o = B_origin(0, 0, z, N);
+        B_r3 = B_ring3(0, 0, z, N, p);
+        delta_z = [delta_z, abs(norm(B_o) - norm(B_r3))/norm(B_o)];
+    end
+    % figure
+    % z = 0:0.002:0.2;
+    % plot(z,delta_z);
+    Delta_z = [Delta_z, sum(delta_z)];
+end
+
+figure 
+p = 1:N/2
+plot(p, Delta_x, p, Delta_z)
