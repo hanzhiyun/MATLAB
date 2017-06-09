e4ecc8e272fb88a21583ea4e737ba8879e42a37b
Hanzhiyun <1160588449@qq.com>
2017-06-09 10:28:53 +0800

    修复了一个会一直叠加误差的BUG

--- Reactor/delta.m
+++ Reactor/delta.m
@@ -3,12 +3,13 @@ function delta = delta(N)
 
 N = 100;
 % p = 34;
-delta_x = [];
-delta_z = [];
+
 Delta_x = [];
 Delta_z = [];
 
 for p = 1:N/2
+    delta_x = [];
+    delta_z = [];
     for x = 0.064:0.002:0.2
         B_o = B_origin(x, 0, 0, N);
         B_r3 = B_ring3(x, 0, 0, N, p);
@@ -18,7 +19,7 @@ for p = 1:N/2
     % x = 0.064:0.002:0.2;
     % plot(x,delta_x);
     Delta_x = [Delta_x, sum(delta_x)];
-    
+        
     for z = 0:0.002:0.2
         B_o = B_origin(0, 0, z, N);
         B_r3 = B_ring3(0, 0, z, N, p);
@@ -30,6 +31,7 @@ for p = 1:N/2
     Delta_z = [Delta_z, sum(delta_z)];
 end
 
-figure 
-p = 1:N/2
-plot(p, Delta_x, p, Delta_z)
+figure;
+p = 1:N/2;
+plot(p, Delta_x, p, Delta_z);
+legend('Delta_x', 'Delta_z');
