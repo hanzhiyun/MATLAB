5050ac3e99e872ca56bc89265e86610d1c94229a
Hanzhiyun <1160588449@qq.com>
2017-09-21 11:03:30 +0800

    Signed-off-by: Hanzhiyun <1160588449@qq.com>

--- Reactor/New/h_and _n/MAPE.m
+++ Reactor/New/h_and _n/MAPE.m
@@ -1,9 +1,10 @@
-function M = MAPE(err, At)
+function M = MAPE(At, Ft)
 % 平均绝对误差百分比的计算，是在一个预测方法的预测精度的测量统计
-n = length(err);
-n1 = length(At);
+n = length(At);
+n1 = length(Ft);
 if n ~= n1
-    disp('Error! The length of err is not equal the length of At');
+    disp('Error! The length of Ft is not equal the length of At');
 end
+err = abs(At - Ft);
 Mt = abs(err./At);
 M = 100*sum(Mt)/n;
--- Reactor/New/h_and _n/main.m
+++ Reactor/New/h_and _n/main.m
@@ -7,13 +7,6 @@ parfor i = 1:31
     n = 90 + 10*i;
     delta_np(n);
 end
-% delta_np(100);
-% delta_np(200);
-% delta_np(300);
-% delta_n(100, 40);
-% delta_n(200, 34);
-% delta_n(200, 70);
-% delta_n(300, 103);
 
 %关闭并行计算
 closematlabpool; 
